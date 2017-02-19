ea34905585a1a2cea51230446dd5278ea8bacc69
Сергей Сейтов <sseitov@iMacSergey.local>
2017-02-20 01:43:01 +0300

    fix

---
+++ depends/build-fdk-ios.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+
+CONFIGURE_FLAGS="--enable-static --with-pic=yes --disable-shared"
+
+ARCHS="armv7 arm64"
+
+# directories
+SOURCE="fdk-aac"
+FAT="libs"
+
+SCRATCH="fdk-scratch"
+# must be an absolute path
+THIN=`pwd`/"fdk-thin"
+
+COMPILE="y"
+LIPO="y"
+
+DEPLOYMENT_TARGET="9.0"
+
+if [ "$*" ]
+then
+	if [ "$*" = "lipo" ]
+	then
+		# skip compile
+		COMPILE=
+	else
+		ARCHS="$*"
+		if [ $# -eq 1 ]
+		then
+			# skip lipo
+			LIPO=
+		fi
+	fi
+fi
+
+if [ "$COMPILE" ]
+then
+	if [ ! `which yasm` ]
+	then
+		echo 'Yasm not found'
+		if [ ! `which brew` ]
+		then
+			echo 'Homebrew not found. Trying to install...'
+                        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+				|| exit 1
+		fi
+		echo 'Trying to install Yasm...'
+		brew install yasm || exit 1
+	fi
+	if [ ! `which gas-preprocessor.pl` ]
+	then
+		echo 'gas-preprocessor.pl not found. Trying to install...'
+		(curl -L https://github.com/libav/gas-preprocessor/raw/master/gas-preprocessor.pl \
+			-o /usr/local/bin/gas-preprocessor.pl \
+			&& chmod +x /usr/local/bin/gas-preprocessor.pl) \
+			|| exit 1
+	fi
+
+	if [ ! -r $SOURCE ]
+	then
+		echo 'fdk-aac source not found. Pulling lib_fdk ...'
+		git clone git://github.com/mstorsjo/fdk-aac.git
+		cd fdk-aac
+		./autogen.sh
+		cd ..
+	fi
+
+	CWD=`pwd`
+	for ARCH in $ARCHS
+	do
+		echo "building $ARCH..."
+		mkdir -p "$SCRATCH/$ARCH"
+		cd "$SCRATCH/$ARCH"
+
+		CFLAGS="-arch $ARCH"
+
+		if [ "$ARCH" = "x86_64" ]
+		then
+		    PLATFORM="iPhoneSimulator"
+		    CFLAGS="$CFLAGS -mios-simulator-version-min=$DEPLOYMENT_TARGET -fembed-bitcode"
+		    HOST="--host=x86_64-apple-darwin"
+		    CPU=
+		else
+		    PLATFORM="iPhoneOS"
+		    CFLAGS="$CFLAGS -mios-version-min=$DEPLOYMENT_TARGET -fembed-bitcode"
+		    HOST="--host=arm-apple-darwin"
+		    CPU="--with-cpu=arm64"
+		fi
+
+		XCRUN_SDK=`echo $PLATFORM | tr '[:upper:]' '[:lower:]'`
+		CC="xcrun -sdk $XCRUN_SDK clang -Werror=unused-command-line-argument"
+		AS="$CWD/$SOURCE/extras/gas-preprocessor.pl $CC"
+		CXXFLAGS="$CFLAGS"
+		LDFLAGS="$CFLAGS"
+
+		$CWD/$SOURCE/configure \
+		    $CONFIGURE_FLAGS \
+		    $HOST \
+		    $CPU \
+		    CC="$CC" \
+		    CXX="$CC" \
+		    CPP="$CC -E" \
+                    AS="$AS" \
+		    CFLAGS="$CFLAGS" \
+		    LDFLAGS="$LDFLAGS" \
+		    CPPFLAGS="$CFLAGS" \
+		    --prefix="$THIN/$ARCH"
+
+		make -j3 install
+		cd $CWD
+	done
+fi
+
+if [ "$LIPO" ]
+then
+	echo "building fat binaries..."
+	mkdir -p $FAT/lib
+	set - $ARCHS
+	CWD=`pwd`
+	cd $THIN/$1/lib
+	for LIB in *.a
+	do
+		cd $CWD
+		lipo -create `find $THIN -name $LIB` -output $FAT/lib/$LIB
+	done
+
+	cd $CWD
+	cp -rf $THIN/$1/include $FAT
+fi
